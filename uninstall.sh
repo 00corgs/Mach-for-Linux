1b31220f87e983fc2e60f07c54aa5ab0e0f6ffa2
00corgs <79515833+00corgs@users.noreply.github.com>
2022-08-16 05:35:39 +0900

    main

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#If you changed the installation directory, change here.
+uninstall="/usr/local/include/"
+
+echo "Installed directory is $uninstall"
+echo "Are you continue?(y|n)"
+read -r yn
+case "$yn" in
+    [yY]*) echo "Removing..." ;;
+    *) echo "Abort"
+    exit 1 ;;
+esac
+
+sudo rm -rf "$uninstall"/mach/
+sudo rm -rf "$uninstall"/mach-o/
+
+echo "Done"
